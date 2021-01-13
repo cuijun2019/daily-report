e7c1272b76397bec5b0b251dbe5c3f1be43bcbb6
cuijun <18319670614@139.com>
2021-01-13 12:45:33 +0800

    Revert "修改项目占比百分比改成小时"
    This reverts commit 79a23d0f

--- src/main/java/com/etone/project/modules/lte/manager/impl/ProjectManager.java
+++ src/main/java/com/etone/project/modules/lte/manager/impl/ProjectManager.java
@@ -12,6 +12,8 @@ import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
